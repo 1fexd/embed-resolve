060fb52382e76f03b24eb26583cb7a185a1a5e4f
1fexd <PGFvy85nC@protonmail.com>
2024-02-11 15:46:28 +0100

    fix: Bump uriparser to 0.0.11

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,7 +11,7 @@ library("fe.embed-resolve") {
 dependencies {
     relocate("com.gitlab.grrfe:gson-ext:11.0.0")
     relocate("com.google.code.gson:gson:2.10.1")
-    relocate("com.github.1fexd:uriparser:0.0.10")
+    relocate("com.github.1fexd:uriparser:0.0.11")
     relocate("com.github.1fexd:tld-lib:1.0.1")
     relocate("com.github.1fexd:signifykt:0.0.5")
 }
