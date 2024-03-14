3b69bc11471e8f36d1555776bfb50801fef99660
1fexd <PGFvy85nC@protonmail.com>
2024-03-14 20:43:30 +0100

    fix: Relocate instead of bundle

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,9 +10,15 @@ library("fe.embed-resolve") {
 }
 
 dependencies {
-    bundle("com.gitlab.grrfe:gson-ext:11.0.0") { minimize = true }
-    bundle("com.google.code.gson:gson:2.10.1") { minimize = true }
-    bundle("com.github.1fexd:uriparser:0.0.11") { minimize = true }
-    bundle("com.github.1fexd:tld-lib:1.0.1") { minimize = true }
-    bundle("com.github.1fexd:signifykt:0.0.5") { minimize = true }
+    relocate("com.gitlab.grrfe:gson-ext:11.0.0")
+    relocate("com.google.code.gson:gson:2.10.1")
+    relocate("com.github.1fexd:uriparser:0.0.11")
+    relocate("com.github.1fexd:tld-lib:1.0.1")
+    relocate("com.github.1fexd:signifykt:0.0.5")
+
+//    bundle("com.gitlab.grrfe:gson-ext:11.0.0") { minimize = true }
+//    bundle("com.google.code.gson:gson:2.10.1") { minimize = true }
+//    bundle("com.github.1fexd:uriparser:0.0.11") { minimize = true }
+//    bundle("com.github.1fexd:tld-lib:1.0.1") { minimize = true }
+//    bundle("com.github.1fexd:signifykt:0.0.5") { minimize = true }
 }
