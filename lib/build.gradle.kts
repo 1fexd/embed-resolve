09b9720feb9f5a42d7ad47ba217248449109788d
1fexd <PGFvy85nC@protonmail.com>
2024-06-25 12:44:05 +0200

    chore: BOM test

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -16,10 +16,12 @@ dependencies {
 //    implementation("com.gitlab.grrfe.gson-ext:core:16.0.0-gson2-koin3")
 //    implementation("fe.uribuilder:uriparser")
 //    implementation("fe.signify:lib")
-    implementation("com.gitlab.grrfe.gson-ext:core:16.0.0-gson2-koin3")
-    implementation("com.github.1fexd:uriparser:0.0.11")
-    implementation("com.github.1fexd:tld-lib:2.1.0")
-    implementation("com.github.1fexd.signifykt:lib:0.0.5")
+    api(platform("com.github.1fexd.super:platform:0.0.1"))
+
+    api("com.gitlab.grrfe.gson-ext:core")
+    api("com.github.1fexd:uriparser")
+    api("com.github.1fexd:tld-lib")
+    api("com.github.1fexd.signifykt:lib")
 
 //    bundle("com.gitlab.grrfe:gson-ext:11.0.0") { minimize = true }
 //    bundle("com.google.code.gson:gson:2.10.1") { minimize = true }
