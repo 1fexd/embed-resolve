5a66e1cf4c98c4965f807072f201b792d0fb36b9
1fexd <PGFvy85nC@protonmail.com>
2025-09-02 07:52:07 +0200

    chore: Build

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,16 +1,9 @@
 import fe.buildsrc.dependency.Grrfe
 import fe.buildsrc.dependency._1fexd
-import fe.buildsrc.publishing.PublicationComponent
-import fe.buildsrc.publishing.asProvider
-import fe.buildsrc.publishing.publish
+
 
 plugins {
-    kotlin("jvm")
-    id("net.nemerosa.versioning")
-    `maven-publish`
 }
-
-group = "fe.embed-resolve"
 
 dependencies {
     implementation("org.jetbrains:annotations:_")
@@ -18,22 +11,8 @@ dependencies {
     api(_1fexd.signify)
     api(Grrfe.std.result.core)
     api(Grrfe.std.uri)
+
     testImplementation(kotlin("test"))
     testImplementation("com.willowtreeapps.assertk:assertk:_")
+//    testImplementation(Grrfe.std.test)
 }
-
-kotlin {
-    jvmToolchain(21)
-}
-
-java {
-    withJavadocJar()
-    withSourcesJar()
-}
-
-publishing.publish(
-    project,
-    group.toString(),
-    versioning.asProvider(project),
-    PublicationComponent.JAVA
-)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
-import java.util.*
-import kotlin.experimental.ExperimentalTypeInference
+import com.gitlab.grrfe.gradlebuild.maybeConfigureIncludingRootRefreshVersions
+import fe.build.dependencies.Grrfe
+import fe.buildsettings.config.MavenRepository
+import fe.buildsettings.extension.configureRepositories
+
+rootProject.name = "embed-resolve"
 
 pluginManagement {
     repositories {
@@ -9,91 +13,48 @@ pluginManagement {
     }
 
     plugins {
-        id("de.fayard.refreshVersions") version "0.60.5"
-        id("net.nemerosa.versioning") version "3.1.0"
-        kotlin("jvm") version "2.0.20"
+        id("de.fayard.refreshVersions") version "0.60.6"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+        id("net.nemerosa.versioning")
+        kotlin("jvm")
     }
-}
 
-plugins {
-    id("de.fayard.refreshVersions")
-}
+    when (val gradleBuildDir = extra.properties["gradle.build.dir"]) {
+        null -> {
+            val gradleBuildVersion = extra.properties["gradle.build.version"]
+            val plugins = extra.properties["gradle.build.plugins"]
+                .toString().trim().split(",")
+                .map { it.trim().split("=") }
+                .filter { it.size == 2 }
+                .associate { it[0] to it[1] }
 
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://maven.mozilla.org/maven2") }
-        mavenLocal()
-    }
-}
-
-rootProject.name = "embed-resolve"
-include(":core")
-
-fun substitute(directory: Any, dependency: String, substitutes: Map<String, String>) {
-    includeBuild(directory) {
-        dependencySubstitution {
-            for ((artifact, project) in substitutes) {
-                substitute(module("$dependency:$artifact")).using(project(":$project"))
+            resolutionStrategy {
+                eachPlugin {
+                    plugins[requested.id.id]?.let { useModule("$it:$gradleBuildVersion") }
+                }
             }
         }
+
+        else -> includeBuild(gradleBuildDir.toString())
     }
 }
-
-@OptIn(ExperimentalTypeInference::class)
-fun Any?.trySubstitute(
-    dependency: String,
-    @BuilderInference builderAction: MutableMap<String, String>.() -> Unit = {},
-) {
-    this?.let { substitute(this, dependency, buildMap(builderAction)) }
+plugins {
+    id("de.fayard.refreshVersions")
+    id("org.gradle.toolchains.foojay-resolver-convention")
+    id("com.gitlab.grrfe.build-settings-plugin")
 }
 
+configureRepositories(MavenRepository.MavenCentral, MavenRepository.Jitpack)
+maybeConfigureIncludingRootRefreshVersions()
 
-fun hasEnv(name: String): Boolean {
-    return System.getenv(name)?.toBooleanStrictOrNull() == true
-}
+extra.properties["gradle.build.dir"]
+    ?.let { includeBuild(it.toString()) }
 
+include(":core")
 
-val isCI = hasEnv("CI")
-val isJitPack = hasEnv("JITPACK")
-
-val localProperties = file("local.properties")
-val devProperties: Properties? = if (localProperties.exists()) {
-    Properties().apply {
-        localProperties.reader().use { load(it) }
-    }
-} else null
-
-val isDev = (devProperties?.get("dev")?.toString()?.toBooleanStrictOrNull() == true)
-
-if (devProperties != null && isDev && (!isCI && !isJitPack)) {
-    devProperties["kotlin-ext.dir"]?.trySubstitute("com.gitlab.grrfe.kotlin-ext") {
-        this["core"] = "core"
-        this["io"] = "io"
-        this["java-time"] = "java-time"
-        this["result-core"] = "result:result-core"
-        this["result-assert"] = "result:result-assert"
-        this["uri"] = "uri"
-    }
-
-    devProperties["gson-ext.dir"].trySubstitute("com.gitlab.grrfe.gson-ext") {
-        this["core"] = "core"
-    }
-
-    devProperties["httpkt.dir"].trySubstitute("com.gitlab.grrfe.httpkt") {
-        this["core"] = "core"
-        this["ext-gson"] = "ext-gson"
-    }
-
-    devProperties["koin-app.dir"].trySubstitute("com.gitlab.grrfe.koin-app") {
-        this["core"] = "core"
-        this["api"] = "api"
-        this["cli-core"] = "cli:cli-core"
-        this["exposed-core"] = "exposed:exposed-core"
-        this["exposed-sqlite"] = "exposed:exposed-sqlite"
+buildSettings {
+    substitutes {
+        trySubstitute(Grrfe.std, properties["kotlin-ext.dir"])
+        trySubstitute(Grrfe.gsonExt, properties["gson-ext.dir"])
     }
 }
